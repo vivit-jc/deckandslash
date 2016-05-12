0d2080b5fb4706cc7f17f090941ac0de2d77bd38
vivit <kazyu.vanilla@gmail.com>
2016-05-13 02:50:51 +0900

    アイテムの位置(どのdivか)によって挙動を🐸ように

--- dands.js
+++ dands.js
@@ -20,46 +20,74 @@ function makeStr(data){
   return str
 }
 
-function makeItem(dom){
-  if(dom.data('data').equip) dom.append('<button>装備</button>')
-  else dom.append('<button>使う</button>')
-}
-
-function init(data){
-  darray = [0,0,0]
-  $("body").on('click', 'button', function(){
-    console.log($(this).parent().data('data'))
-    if($(this).parent().data('data').type == 'item') console.log("item")
-    else console.log("click")
-  })
-  $("div#dungeon").on('click', "div.card", function(s){
-  })
-  data.forEach(function(val,index,ar){
-    darray[index] = shuffle(val)
-  })
-  var d = $.data($('div#dungeon'),"deck",darray)
-  for(var i=0;i<5;i++){
-    carddata = d[0].pop()
-    divcard = $("<div class=card></div>")
-    divcard.css("left",9+i*100)
-    str = makeStr(carddata)
-    divcard.html(str)
-    divcard.data('data',carddata)
-    $("div#dungeon").append(divcard)
-  }
-  $("div.card").click(function(){
-    data = $(this).data('data')
-    if(data.type=="item") {
-      makeItem($(this))
-//ここ      console.log($(this).parent("div#dungeon"))
-      if($(this).parent() == $("dungeon")){
-        $("div#bag").append($(this).clone(true))
-        $(this).remove()
-      }
+function makeItem($dom) {
+    if ($dom.find('button').length === 0) {
+        if ($dom.attr('data-equip')) {
+            $dom.append('<button>装備</button>');
+        } else {
+            $dom.append('<button>使う</button>');
+        }
+    } else {
+        $dom.find('button').remove();
     }
-  })
 }
 
-$(document).ready(function(){
-  $.getJSON("data.json", function(data) {init(data)})
-})
+
+var bindEvents = function () {
+    $("body").on('click', 'button', function () {
+        var $btn = $(this);
+
+        if ($btn.parent().attr('data-type') === 'item') {
+            console.log("item");
+        } else {
+            console.log("click");
+        }
+    });
+
+    // 動的に生成されたdiv.card要素のクリックイベントを監視    
+    $("body").on('click', 'div.card', function (e) {
+        var $card = $(e.target).closest('div.card');
+        if ($card.attr('data-type') === "item") {
+            //ここ console.log($(this).parent("div#dungeon"))
+            if ($card.parent().attr("id") === "dungeon") {
+                $card.remove();
+                makeItem($card);
+                $("div#bag").append($card.clone(true));
+            } else if ($card.parent().attr("id") === "bag") {
+                $card.remove();
+                makeItem($card);
+                $("div#dungeon").append($card.clone(true));
+            }
+        }
+    });
+};
+
+var init = function (data) {
+    var darray = [0, 0, 0];
+    data.forEach(function (val, index, ar) {
+        darray[index] = shuffle(val);
+    });
+    console.info(darray);
+    //var d = $.data($('div#dungeon'), "deck", darray);
+
+    for (var i = 0; i < 5; i++) {
+        var carddata = darray[0].pop();
+        var carddataKeys = Object.keys(carddata);
+        var $divcard = $("<div class=card></div>");
+        $divcard.css("left", 9 + i * 100);
+        str = makeStr(carddata);
+        $divcard.html(str);
+        // DOMのdataset属性にカード情報を持たせる
+        carddataKeys.forEach(function (key) {
+            $divcard.attr('data-' + key, carddata[key]);
+        });
+        $("div#dungeon").append($divcard);
+    }
+};
+
+$(function () {
+    bindEvents();
+    $.getJSON("data.json", function (data) {
+        init(data);
+    });
+});
