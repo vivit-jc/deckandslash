7b123d9247820df4a350f8769fae3bbb06644666
vivit <kazyu.vanilla@gmail.com>
2016-05-22 02:42:45 +0900

    モンスターとの戦闘と、次に進む処理を実装

--- dands.js
+++ dands.js
@@ -10,7 +10,7 @@ function shuffle(array) {
 }
 
 function makeStr(data){
-    str = data.name
+    var str = data.name
     if(data.type=="monster"){
         str += "<br>POW: "+"<span class=monpower>"+data.power+"</span>"
         if(data.sub) str += "<br>"+data.sub
@@ -20,14 +20,64 @@ function makeStr(data){
     return str+"<br>"
 }
 
-function calcDamage(escape){
+function execBattle(){
+    $('span#hp').html(+$('span#hp').html() - $('span#damage').html())
+    $('div#dungeon div.card').each(function(i,v){
+        if($(v).attr('data-type') != "monster") return true
+        var $card = $(v)
+        if($card.children('input.escape').is(':checked')){
+            $("div#trash").append($card.clone(true));
+        } else {
+            $("div#removed").append($card.clone(true));
+        }
+        $card.remove()
+    })
+    calcDamage()
+    $('button#next').text("進む")
+}
+
+function drawCards(){
+    $('div#dungeon div.card').each(function(i,v){
+        $(v).remove()
+        $('div#trash').append($(v).clone(true))
+    })
+    for (var i = 0; i < 5; i++) {
+        var carddata = darray[0].pop();
+        var carddataKeys = Object.keys(carddata);
+        var $divcard = $("<div class=card></div>");
+        $divcard.css("left", 9 + i * 100);
+        var str = makeStr(carddata);
+        $divcard.html(str);
+        if(carddata.type=="monster"){
+            $divcard.append("逃げる<input type=checkbox class=escape>")
+        }
+        // DOMのdataset属性にカード情報を持たせる
+        carddataKeys.forEach(function (key) {
+            $divcard.attr('data-' + key, carddata[key]);
+        });
+        $("div#dungeon").append($divcard);
+    }
+    calcDamage()
+    $('button#next').text("戦う")
+}
+
+function calcDamage(){
     var damage = 0
-    $('div.card').each(function(i,v){
+    var escape = getEscapedMonster()
+    $('div#dungeon div.card').each(function(i,v){
         if($(v).attr('data-type') != "monster") return true
         if($(v).children('input.escape').is(':checked')) return true
         damage += +$(v).children('.monpower').html()
     })
     $('span#damage').html(damage+escape)
+    if(damage+escape >= +$('span#hp').html()){
+        $('span#damage').css('color','red')
+        $('span#damage').css('font-weight','bold')
+    } else {
+        $('span#damage').css('color','black')
+        $('span#damage').css('font-weight','')
+    }
+    $('span#escape').html(getEscapedMonster()+"/"+getMaxEscape())
 }
 
 function addButtonToItem($dom) {
@@ -57,13 +107,19 @@ function getMaxEscape(){
     return 2
 }
 
+function getEscapedMonster(){
+    return $('div#dungeon input.escape:checked').length
+}
+
 var bindEvents = function () {
-    $("body").on('click', 'button', function () {
-        var $btn = $(this);
-        if ($btn.attr("class") === "drop") {
-            dropItem($btn.closest('div.card'))
-        }
+    $("body").on('click', 'button.drop', function (dom) {
+        dropItem(getSelfCard(dom))
     });
+
+    $("body").on('click', 'button#next', function (dom) {
+        if($(dom.target).html() == "戦う") execBattle()
+        else if($(dom.target).html() == "進む") drawCards()
+    })
 
     // 動的に生成されたdiv.card要素のクリックイベントを監視
     $("body").on('click', 'div.card', function (e) {
@@ -77,50 +133,36 @@ var bindEvents = function () {
         }
     });
 
+    // 逃げるチェックボックスのクリックイベント
     $("body").on('click', 'input.escape', function(e) {
         var $card = getSelfCard(e)
-        var escape = $('input.escape:checked').length
-        if($('input.escape:checked').length >= getMaxEscape()){
-            $('input.escape:not(:checked)').each(function (i,v) {
+        var escape = getEscapedMonster()
+        if($('div#dungeon input.escape:checked').length >= getMaxEscape()){
+            $('div#dungeon input.escape:not(:checked)').each(function (i,v) {
                 $(v).prop('disabled', true)
             })
         } else {
-            $('input.escape:disabled').each(function (i,v) {
+            $('div#dungeon input.escape:disabled').each(function (i,v) {
                 $(v).prop('disabled', false)
             })
         }
-        calcDamage(escape)
+        calcDamage()
     })
 };
 
 var init = function (data) {
-    var darray = [0, 0, 0];
     data.forEach(function (val, index, ar) {
         darray[index] = shuffle(val);
     });
     console.info(darray);
-
-    for (var i = 0; i < 5; i++) {
-        var carddata = darray[0].pop();
-        var carddataKeys = Object.keys(carddata);
-        var $divcard = $("<div class=card></div>");
-        $divcard.css("left", 9 + i * 100);
-        str = makeStr(carddata);
-        $divcard.html(str);
-        if(carddata.type=="monster"){
-          $divcard.append("逃げる<input type=checkbox class=escape>")
-        }
-        // DOMのdataset属性にカード情報を持たせる
-        carddataKeys.forEach(function (key) {
-            $divcard.attr('data-' + key, carddata[key]);
-        });
-        $("div#dungeon").append($divcard);
-    }
+    drawCards()
 };
 
 $(function () {
+    darray = [0, 0, 0]
     bindEvents();
     $.getJSON("data.json", function (data) {
         init(data);
+        calcDamage();
     });
 });
